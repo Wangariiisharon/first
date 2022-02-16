5554a560d199d1c1025b5430647cbe5139a5bd28
Wangariiisharon <mwangiiisharon@gmail.com>
2022-02-16 20:59:37 +0300

    First Report

---
+++ Assignment/Assignment.kt
@@ -0,0 +1,16 @@
+fun main () {
+ var name:string="Mary Njeri"
+    println(name)
+    var number: string="0700620110"
+    println(number)
+    var age:int=12
+    println(age)
+    var weight:int=60
+     println(weight)
+    var kenyan:boolean=true
+    println(kenyan)
+
+
+
+
+}
---
+++ untitled/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var name = arrayOf("Jane", "Mercy", "John", "Asher")
+println(name[0])
+    println(name[1])
+    println(name[2])
+    println(name[3])
+
+
+}
