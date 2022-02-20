f4e3a7fa220f18bc0ff207099c844472dea62173
Wangariiisharon <mwangiiisharon@gmail.com>
2022-02-20 22:51:04 +0300

    my second

---
+++ second assignment/src/Main.kt
@@ -0,0 +1,22 @@
+fun main(){
+    var greetings ="Hello Saint"
+    println(greetings)
+    var rem = rem(20,3)
+    println(rem)
+    var sum= sum(8,5,3,4)
+    println(sum)
+    fact()
+
+}
+fun rem(a:Int, b:Int): Int{
+    var rem = a % b
+    return rem
+}
+fun sum(w:Int, x:Int, y:Int, z:Int) :Int {
+var sum= w+x+y+z
+return(sum)
+}
+fun fact(){
+    var fact= ("I can sing")
+    println(fact)
+    }
